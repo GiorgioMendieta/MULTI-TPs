37012ea8eb5c3700b5e89b9f8d48acd3ef9fb3c2
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-02-04 19:34:33 +0100

    Add CCFLAGS variable

--- tp2/tp2_etudiant/compilation.sh
+++ tp2/tp2_etudiant/compilation.sh
@@ -13,6 +13,9 @@ export CC=/opt/gcc-cross-mipsel/8.2.0/bin/mipsel-unknown-elf-gcc
 export LD=/opt/gcc-cross-mipsel/8.2.0/bin/mipsel-unknown-elf-ld
 export DU=/opt/gcc-cross-mipsel/8.2.0/bin/mipsel-unknown-elf-objdump
 
+export CCFLAGS="-Wall -mno-gpopt -ffreestanding -mips32 -I$(GIET_SYS_PATH) -I."
+
+
 # Change directory 
 cd ./soft
 
@@ -21,9 +24,9 @@ $AS -g -mips32 -o reset.o reset.s
 $AS -g -mips32 -o giet.o $GIET_SYS_PATH/giet.s
 
 # Compile C files
-$CC -Wall -mno-gpopt -ffreestanding -mips32 -I$GIET_SYS_PATH -I. -c -o drivers.o $GIET_SYS_PATH/drivers.c
-$CC -Wall -mno-gpopt -ffreestanding -mips32 -I$GIET_SYS_PATH -I. -c -o common.o $GIET_SYS_PATH/common.c
-$CC -Wall -mno-gpopt -ffreestanding -mips32 -I$GIET_SYS_PATH -I. -c -o ctx_handler.o $GIET_SYS_PATH/ctx_handler.c
-$CC -Wall -mno-gpopt -ffreestanding -mips32 -I$GIET_SYS_PATH -I. -c -o irq_handler.o $GIET_SYS_PATH/irq_handler.c
-$CC -Wall -mno-gpopt -ffreestanding -mips32 -I$GIET_SYS_PATH -I. -c -o sys_handler.o $GIET_SYS_PATH/sys_handler.c
-$CC -Wall -mno-gpopt -ffreestanding -mips32 -I$GIET_SYS_PATH -I. -c -o exc_handler.o $GIET_SYS_PATH/exc_handler.c
+$CC $CCFLAGS -c -o drivers.o $GIET_SYS_PATH/drivers.c
+$CC $CCFLAGS -c -o common.o $GIET_SYS_PATH/common.c
+$CC $CCFLAGS -c -o ctx_handler.o $GIET_SYS_PATH/ctx_handler.c
+$CC $CCFLAGS -c -o irq_handler.o $GIET_SYS_PATH/irq_handler.c
+$CC $CCFLAGS -c -o sys_handler.o $GIET_SYS_PATH/sys_handler.c
+$CC $CCFLAGS -c -o exc_handler.o $GIET_SYS_PATH/exc_handler.c
