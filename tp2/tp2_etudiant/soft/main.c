3cdc110eb76937a7b1933ca74f5f5b6b6ae51b19
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-02-04 16:13:49 +0100

    Complete main function

--- tp2/tp2_etudiant/soft/main.c
+++ tp2/tp2_etudiant/soft/main.c
@@ -2,12 +2,13 @@
 
 __attribute__((constructor)) void main()
 {
-	char		c;
-	char		s[] = "\n Hello World! \n";
+	char c;
+	char s[] = "\n Hello World! \n";
 
-	while(1) 
-        {
-            TO BE COMPLETED
+	while (1)
+	{
+		tty_puts(s); // This function displays a string on a terminal.
+		tty_getc(c); // This blocking function fetches a single ascii character from a terminal
 	}
 
 } // end main
