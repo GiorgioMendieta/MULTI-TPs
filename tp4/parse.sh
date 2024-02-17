b48520748a029e421cfc8dab4eff2f760e7fd6dd
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-02-17 02:03:30 +0100

    Update scripts

--- tp4/parse.sh
+++ tp4/parse.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# cd to where the Stats are stored
+cd Stats
+
+# CSV headers
 headers="CYCLE,IMISS RATE,DMISS RATE,CPI"
 
 for dir in */; do
     # Set the output CSV file name for each directory
+    # Remove the characters after '%'
     results="${dir%/}_results.csv"
     
     # Remove output file if it exists
@@ -16,7 +21,7 @@ for dir in */; do
     for input_file in $dir/*.txt; do
 
         # Set the output CSV file name based on the input file name
-        # %.txt removes the extension
+        # Remove the characters after '%'
         csv_file="${input_file%.txt}.csv"
 
         # Remove output file if it exists
@@ -26,7 +31,13 @@ for dir in */; do
         echo $headers > $csv_file
 
         # Use awk to extract the values and append them to the CSV file
-        awk '/^[-*]/ {if (/cycle/) cycle=$NF; if (/IMISS RATE/) imr=$NF; if (/DMISS RATE/) dmr=$NF; if (/CPI/) cpi=$NF; if (/^\*\*\* proc/) print cycle "," imr "," dmr "," cpi}' "$input_file" >> "$csv_file"
+        awk '/^[-*]/ { \
+                if (/cycle/) cycle=$NF;  \
+                if (/IMISS RATE/) imr=$NF;  \
+                if (/DMISS RATE/) dmr=$NF;  \
+                if (/CPI/) cpi=$NF;  \
+                if (/^\*\*\* proc/) print cycle "," imr "," dmr "," cpi}' \
+            "$input_file" >> "$csv_file"
     
         # Get the last line of the CSV file and append it to the results CSV
         tail -1 "$csv_file" >> "$results"
