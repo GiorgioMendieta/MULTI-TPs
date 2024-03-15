bc2b630e0552a5826171445127dc7350531f52a7
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2024-03-15 16:52:50 +0100

    Update syscalls for their DMA counterparts

--- tp7/soft/main_dma.c
+++ tp7/soft/main_dma.c
@@ -32,7 +32,7 @@ __attribute__((constructor)) void main()
 
         tty_printf(" - build   OK at cycle %d\n", proctime());
 
-        if (fb_sync_write(0, BUF, NLINE * NPIXEL) != 0)
+        if (fb_write(0, BUF, NLINE * NPIXEL) | fb_completed() != 0)
         {
             tty_printf("\n!!! error in fb_syn_write syscall !!!\n");
             exit();
